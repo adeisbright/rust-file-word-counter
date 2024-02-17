4fcdd23f95bfa45f9b01fa19f2c84f9307f62108
Adeleke Bright <bigjaratechnologies@gmail.com>
2024-02-17 23:56:13 +0100

    Add File Reading Standard Crates

--- src/filereader/reader.rs
+++ src/filereader/reader.rs
@@ -1,3 +1,12 @@
+use std::fs::File ; 
+use std::path::Path ; 
+
 pub fn file_reader(file_name : &String) -> &String{
+    let file_path = Path::new(file_name);
+    let display = file_path.display();
+    let file = match File::open(file_path){
+        Ok(filer) => filer ,
+        Err(why) => panic!("No file as {} found {}" , file_name , why),
+    };
     file_name
 }

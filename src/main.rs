136ed4f9dc3c5a7e4bf32466d1af6725f8918066
Adeleke Bright <bigjaratechnologies@gmail.com>
2024-02-18 23:57:22 +0100

    Reading Arguments From the Command Line as File Name

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 extern crate  rust_file_word_counter ;
 use rust_file_word_counter::filereader::reader ; 
-
+use std::env ; 
 fn main() {
     println!("Hello, world!");
+    //Collect values from the environment 
+    let arguments : Vec<String> = env::args().collect() ; 
+    //Reference the file name 
+    let file_name = &arguments[1] ; 
+
     //let the_file = String::from("hello.txt");
     //let my_file = reader::file_reader(&the_file);
     //print!("This is my file name {}" , my_file);
-    reader::file_reader("Cargo.toml");
+    reader::file_reader(&file_name);
 }

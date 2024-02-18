443e1fc6b953f893966c4a0cb9663871b0a08fd9
Adeleke Bright <bigjaratechnologies@gmail.com>
2024-02-19 00:36:11 +0100

    Rename Struct Method and Improve Error Handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate  rust_file_word_counter ;
 use rust_file_word_counter::filereader::reader ; 
-use std::env ; 
+use std::{env, process} ; 
 fn main() {
-    println!("Hello, world!");
     //Collect values from the environment 
     let arguments : Vec<String> = env::args().collect() ; 
-    let config = Config::new(&arguments);
+    let config = Config::build(&arguments)
+    .unwrap_or_else(|err|{
+        println!("Problems with reading arguments {err}");
+        process::exit(1)
+    }) ;
     reader::file_reader(config.file_name);
 } 
 
@@ -13,8 +16,11 @@ struct Config {
     file_name :String ,
 }
 impl  Config {
-    fn new(args : &[String]) -> Config{
+    fn build(args : &[String]) -> Result<Config , &'static str>{
+        if args.len() < 1 {
+            return Err("Not Enough Argument Error : Please provide file name");
+        }
         let file_name = args[1].clone() ; 
-        Config {file_name}
+        Ok(Config {file_name})
     }
 }
